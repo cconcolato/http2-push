0db8fa0d11dcca8c6b61d2cb321dbcacf56795ea
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-05-04 13:43:20 +0200

    add 3rd test

--- xhr-onpush.js
+++ xhr-onpush.js
@@ -1,6 +1,6 @@
 var xhr;
 xhr = new XMLHttpRequest();
-xhr.open("GET", "https://localhost:8091/file1.txt?push=file2.txt", true);
+xhr.open("GET", "https://localhost:8091/file1.txt?push=file2.txt&push=file3.txt", true);
 xhr.onload = function () {
 	console.log("xhr state="+this.readyState+" status="+this.status+" response:", this.response);
 	document.getElementById('debug').innerHTML = this.response+"<br>";
@@ -12,8 +12,20 @@ xhr.onload = function () {
 	}
 	xhr2.onload = function () {
 		console.log("xhr2 onload state="+this.readyState+" status="+this.status+" response:", this.response);
-		document.getElementById('debug').innerHTML += this.response;
+		document.getElementById('debug').innerHTML += this.response+"<br>";
 	}
 	xhr2.send();
+
+	var xhr3 = new XMLHttpRequest();
+	xhr3.open("GET", "https://localhost:8091/file3.txt", true);
+	xhr3.onreadystatechange = function () {
+		console.log("xhr3 onchange state="+this.readyState+" status="+this.status+" response:", this.response);
+	}
+	xhr3.onload = function () {
+		console.log("xhr3 onload state="+this.readyState+" status="+this.status+" response:", this.response);
+		document.getElementById('debug').innerHTML += this.response+"<br>";
+	}
+	xhr3.send();
+
 }
 xhr.send();
