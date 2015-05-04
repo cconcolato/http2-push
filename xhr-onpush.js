313bf527538e30401b24c14eafe7224a12bc1c71
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-05-04 13:26:02 +0200

    adding client XHR to test pushed resources

---
+++ xhr-onpush.js
@@ -0,0 +1,17 @@
+var xhr;
+xhr = new XMLHttpRequest();
+xhr.open("GET", "https://localhost:8091/file1.txt?push=file2.txt", true);
+xhr.onload = function () {
+	console.log("xhr state="+this.readyState+" status="+this.status+" response:", this.response);
+
+	var xhr2 = new XMLHttpRequest();
+	xhr2.open("GET", "https://localhost:8091/file2.txt", true);
+	xhr2.onreadystatechange = function () {
+		console.log("xhr2 onchange state="+this.readyState+" status="+this.status+" response:", this.response);
+	}
+	xhr2.onload = function () {
+		console.log("xhr2 onload state="+this.readyState+" status="+this.status+" response:", this.response);
+	}
+	xhr2.send();
+}
+xhr.send();
