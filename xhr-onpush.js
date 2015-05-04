b679d03d2daff9925892d8bfcf0c3965b2893797
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-05-04 13:39:37 +0200

    display XHR/PP content in HTML

--- xhr-onpush.js
+++ xhr-onpush.js
@@ -3,6 +3,7 @@ xhr = new XMLHttpRequest();
 xhr.open("GET", "https://localhost:8091/file1.txt?push=file2.txt", true);
 xhr.onload = function () {
 	console.log("xhr state="+this.readyState+" status="+this.status+" response:", this.response);
+	document.getElementById('debug').innerHTML = this.response+"<br>";
 
 	var xhr2 = new XMLHttpRequest();
 	xhr2.open("GET", "https://localhost:8091/file2.txt", true);
@@ -11,6 +12,7 @@ xhr.onload = function () {
 	}
 	xhr2.onload = function () {
 		console.log("xhr2 onload state="+this.readyState+" status="+this.status+" response:", this.response);
+		document.getElementById('debug').innerHTML += this.response;
 	}
 	xhr2.send();
 }
